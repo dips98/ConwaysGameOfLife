8d7035a1b8d5b9265bad59027dd27454cf2c05d7
dips98 <62205185+dips98@users.noreply.github.com>
2020-10-01 04:30:41 +0530

    Update main.py
    Just Improved readability of code rest as it is

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import time
 import random
 from objects import *
 
-height=1000
-width=1000                                          #default height and Width of screen
+height = 1000
+width = 1000                                        #default height and Width of screen
 pygame.init()
 print('''Controls:\n
 Space : Start/Pause Simulation
 R: Make Random Pattern
 Left Click : Make Cell Alive/Dead''')
-screen=pygame.display.set_mode((height,width))
-cellSize=20                                         #default Cell Size 
-g=grid(cellSize,height,width,screen)                #making Grid with cellSize, Width and Height
+screen = pygame.display.set_mode((height, width))
+cellSize = 20                                       #default Cell Size 
+g = grid(cellSize, height, width, screen)           #making Grid with cellSize, Width and Height
 mouseX, mouseY = pygame.mouse.get_pos()
-global drawing,Genrations                           
-drawing=True
-Genrations=0
+global drawing, Genrations                           
+drawing = True
+Genrations = 0
 
 def drawAgain():                                    #udating Graphics On Screen
     for row in g.listOfCells:
@@ -26,39 +26,39 @@ def drawAgain():                                    #udating Graphics On Screen
 def makeRandomPattern(listOfCells):                 #function To Genrate Random Grid Pattern 
     for i in range(len(listOfCells)):
         for j in range(len(listOfCells)):
-            if random.randint(0,100)%2==0:
-                listOfCells[i][j].isAlive=True
+            if random.randint(0,100) % 2 == 0:
+                listOfCells[i][j].isAlive = True
                 listOfCells[i][j].update()
                 pygame.display.flip()
 
 def up_Date():                                      #updating Calculation
-    global drawing,Genrations
-    revent =pygame.event.get()
+    global drawing, Genrations
+    revent = pygame.event.get()
     for e in revent:
-        if e.type== pygame.MOUSEBUTTONDOWN and drawing:
+        if e.type == pygame.MOUSEBUTTONDOWN and drawing:
             x, y = pygame.mouse.get_pos()
-            g.listOfCells[x//cellSize][y//cellSize].isAlive=not g.listOfCells[x//cellSize][y//cellSize].isAlive
+            g.listOfCells[x // cellSize][y // cellSize].isAlive = not g.listOfCells[x // cellSize][y // cellSize].isAlive
                                                     #making Cell ALive On Selected Position
-        if e.type== pygame.KEYDOWN :
-            CurrentEvent=e
-            if CurrentEvent.key==pygame.K_SPACE:
-                drawing=not drawing
-            if CurrentEvent.key==pygame.K_r:
+        if e.type == pygame.KEYDOWN:
+            CurrentEvent = e
+            if CurrentEvent.key == pygame.K_SPACE:
+                drawing = not drawing
+            if CurrentEvent.key == pygame.K_r:
                 makeRandomPattern(g.listOfCells)
-    if(not drawing):
+    if not drawing:
         for row in g.listOfCells:                   #calulating For Each Cell In the Grid
             for cell in row:
                 cell.canThisCellBorn(g.listOfCells)
         for row in g.listOfCells:                   #setting Next Genration
             for cell in row:
                 cell.updateGenration()
-        Genrations=Genrations+1   
-        print("Genration :",Genrations)
+        Genrations += 1   
+        print("Genration :", Genrations)
         pygame.event.get() 
             
 
 while True:                                         #running Whole Program In loop
-    event=pygame.event.get() 
+    event = pygame.event.get() 
     drawAgain()                                     #drawing 
     up_Date()                                       #updating Calculations
     if not drawing:
